792be2564f50958057cd3d222b8009ccab2224b1
Axel Haslam <axelx.haslam@intel.com>
2012-03-23 10:26:42 +0100

    mki.sh search update
    modified:   mki.sh
    modified:   search
    
    Signed-off-by: Axel Haslam <axelx.haslam@intel.com>

--- mki.sh
+++ mki.sh
@@ -1,17 +1,34 @@
 DIR=~/r3
-OUT=out/target/product/mfld_pr2
 
 # R3 INFO #
 R3_DIR=~/r3
-R3_RELEASE=2012_WW08
+R3_RELEASE=2012_WW11
 R3_URL=jfumgbuild-depot.jf.intel.com/build/eng-builds/mfld-r3/android/ice-cream-sandwich-platform/releases
 R3_MANIFEST=$R3_URL/$R3_RELEASE/manifest-$R3_RELEASE-generated.xml
 
 # R2 INFO #
 R2_DIR=~/r2
-R2_RELEASE=2012_WW08
+R2_RELEASE=2012_WW11
 R2_URL=jfumgbuild-depot.jf.intel.com/build/eng-builds/mfld-r2/android/gingerbread-platform/releases
 R2_MANIFEST=$R2_URL/$R2_RELEASE/manifest-$R2_RELEASE-generated.xml
+
+# R3 INFO #
+CV_DIR=~/r3
+CV_RELEASE=2012_WW11
+CV_URL=jfumgbuild-depot.jf.intel.com/build/eng-builds/mfld-r3/android/ice-cream-sandwich-platform/releases
+CV_MANIFEST=$CV_URL/$CV_RELEASE/manifest-$CV_RELEASE-generated.xml
+
+
+PLATFORM=ctp_pr0
+OUT=out/target/product
+OUT_PLATFORM=$OUT/$PLATFORM
+
+rm -rf $R2_DIR
+rm -rf $R3_DIR
+
+ln -s /home/axelh/RELEASES/R3/$R3_RELEASE ~/r3
+ln -s /home/axelh/RELEASES/R2/$R2_RELEASE ~/r2
+
 
 #START A FULL BUIL
 print(){
@@ -26,108 +43,88 @@ set_dir()
 		DIR=$R3_DIR
 	elif [  "$1" = "r2" ]; then
 		DIR=$R2_DIR
+	elif [  "$1" = "cv" ]; then
+		DIR=$R3_DIR
 	else
 		DIR=$R3_DIR
 	fi
-	cd $DIR
-
 }
 
 full_andrid_build(){
 	set_dir $1
+	cd $DIR
+	
+	case $1 in 
+	r2)	
+		PLATFORM=mfld_pr2
+		;;
+	r3)	
+		PLATFORM=mfld_pr2
+		;;
+	cv)     
+		PLATFORM=ctp_pr0
+		;;
+	*)exit 1;;
+	esac
+
         source build/envsetup.sh
-        lunch mfld_pr2-eng
-        make -j8 mfld_pr2
+        lunch $PLATFORM-eng
+        make  $PLATFORM
 }
 
 build_boottarball(){
+	set_dir $1
 	cd $DIR
-
-	rm -rf $DIR/out/target/product/mfld_pr2/boot.bin
-	rm -rf $DIR/out/target/product/mfld_pr2/kernel_build/arch/x86/boot/bzImage
-	rm -rf $DIR/out/target/product/mfld_pr2/kernel_build/arch/i386/boot/bzImage
-	rm -rf $DIR/out/target/product/mfld_pr2/bzImage
+	rm -rf $DIR/$OUT_PLATFORM/boot.bin
+	rm -rf $DIR/$OUT_PLATFORM/kernel_build/arch/x86/boot/bzImage
+	rm -rf $DIR/$OUT_PLATFORM/kernel_build/arch/i386/boot/bzImage
+	rm -rf $DIR/$OUT_PLATFORM/bzImage
 	source build/envsetup.sh
-	lunch mfld_pr2-eng
+	lunch $PLATFORM-eng
 	make -j4 boottarball	
 }
 
-build_r3(){
+incremental_kernel(){
+	set_dir $1
+	print "BUILDING KERNEL FOR: $1 PLATFORM: $PLATFORM"
+
 	cd $DIR
-	rm -rf $DIR/$OUT/kernel_build/arch/x86/boot/bzImage  $DIR/$OUT/boot/kernel
-	$DIR/vendor/intel/support/kernel-build.sh -c mfld_pr2 -o $DIR/$OUT/kernel_build
-	if [ ! -e $DIR/$OUT/kernel_build/arch/x86/boot/bzImage ]; then
+	rm -rf $DIR/$OUT_PLATFORM/kernel_build/arch/x86/boot/bzImage  $DIR/$OUT_PLATFORM/boot/kernel
+	$DIR/vendor/intel/support/kernel-build.sh -c $PLATFORM -o $DIR/$OUT_PLATFORM/kernel_build
+	if [ ! -e $DIR/$OUT_PLATFORM/kernel_build/arch/x86/boot/bzImage ]; then
 		print "######  BUILD ERROR #####"
 	exit
 	fi
-
+	CTP_CMDLINE="init=/init pci=noearly console=ttyS0 earlyprintk=mrst loglevel=8 hsu_dma=7 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=ctp_pr0 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on"
+	MFLD_CMDLINE="init=/init pci=noearly console=ttyMFD3 console=logk0 earlyprintk=nologger loglevel=8 hsu_dma=7 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=mfld_pr2 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on apic=debug"
 	
-	vendor/intel/support/mkbootimg --cmdline "init=/init pci=noearly console=ttyMFD3 console=logk0 earlyprintk=nologger loglevel=4 hsu_dma=7 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=mfld_pr2 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on apic=debug"  --ramdisk $OUT/boot/ramdisk.img \
-	--kernel $OUT/kernel_build/arch/i386/boot/bzImage \
-	--output $OUT/boot.bin \
-	--product mfld_pr2 \
+	vendor/intel/support/mkbootimg --cmdline $MFLD_CMDLINE  --ramdisk $OUT_PLATFORM/boot/ramdisk.img \
+	--kernel $OUT_PLATFORM/kernel_build/arch/i386/boot/bzImage \
+	--output $OUT_PLATFORM/boot.bin \
+	--product $PLATFORM \
 	--type mos
 	
 }
-
-build_r2(){
-	cd $DIR
-	rm -rf $DIR/$OUT/kernel_build/arch/x86/boot/bzImage  $DIR/$OUT/boot/kernel
-	$DIR/vendor/intel/support/kernel-build.sh -c mfld_pr2 -o $DIR/$OUT/kernel_build
-	if [ ! -e $DIR/$OUT/kernel_build/arch/x86/boot/bzImage ]; then
-		print "######  BUILD ERROR #####"
-		exit
-	fi
-
-	cp $DIR/$OUT/kernel_build/arch/x86/boot/bzImage  $DIR/$OUT/boot/kernel
-	vendor/intel/support/mkbootimg --cmdline "init=/init pci=noearly console=ttyMFD3 earlyprintk=nologger loglevel=8 hsu_dma=7 kmemleak=off androidboot.bootmedia=sdcard androidboot.hardware=mfld_pr2 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on" \
-	--ramdisk $OUT/boot/ramdisk.img \
-	--kernel $OUT/kernel_build/arch/i386/boot/bzImage \
-	--output $OUT/boot.bin \
-	--product mfld_pr2 \
-	--type mos
-}
-
-reboot(){
-	print "REBOOT"
-	adb shell "update_osip --backup --invalidate 1; reboot"
-#	adb reboot-bootloader
-	
-
-}
-
-flash_boot()
-{
-	print "FLASH"
-	sudo fastboot flash boot $DIR/out/target/product/mfld_pr2/boot.bin
-	sudo fastboot continue
-}
-
-
 
 build_kernel()
 {
 	reboot
 	print "BUILDING $1"
+	case $1 in
+	r2)incremental_kernel $1;;
+	r3)incremental_kernel $1;;
+	cv)incremental_kernel $1;;
+	boottarball_r3)build_boottarball $1;;
+	boottarball_r2)build_boottarball $1;;
+	boottarball_cv)build_boottarball $1;;
+	*) print "build_kernel: error!"
+	esac
+}
 
-	if [ "$1" = "r3" ]; then
-		DIR=$R3_DIR
-		build_r3
-	elif [  "$1" = "r2" ]; then
-		DIR=$R2_DIR
-		build_r2
-	elif [ "$1" = "boottarball_r3" ]; then
-		DIR=$R3_DIR
-		build_boottarball
-	elif [ "$1" = "boottarball_r2" ]; then
-		DIR=$R2_DIR
-		build_boottarball	
-	else
-		print " BUILDING DEFAULT R3"
-		DIR=$R3_DIR
-		build_r3
-	fi
-	flash_boot
+reboot(){
+	print "REBOOT"
+	adb shell "update_osip --backup --invalidate 0; update_osip --backup --invalidate 1;reboot"
+#	adb reboot-bootloader
 }
 
 flash_kernel()
@@ -135,22 +132,10 @@ flash_kernel()
 	reboot
 	set_dir $1
 	cd $DIR
+	print "FLASH"
+	sudo fastboot flash boot $DIR/$OUT/boot.bin
+	sudo fastboot continue
 	flash_boot
-}
-
-flash()
-{
-	cd ~/DB/CURRENT/IMAGE
-	fastboot oem system /sbin/PartitionDisk.sh /dev/mmcblk0
-	
-        sudo fastboot flash boot boot.bin
-        sudo fastboot flash recovery recovery.img
-        sudo fastboot flash radio radio_firmware.bin
-        sudo fastboot erase system
-        sudo fastboot flash system system.tar.gz
-        sudo fastboot flash dnx SIGNED_PNW_C0_D0_FWR_DnX.FD.24.bin
-        sudo fastboot flash ifwi IFWI_v02.16_CRAK.bin
-
 }
 
 #GET DAILY BUILD AND FLASHIT
@@ -164,12 +149,24 @@ get(){
 		THIS_RELEASE=$R2_RELEASE
 		THIS_URL=$R2_URL
 		THIS_DIR=~/DB/R2/$R2_RELEASE
+		PLAT_DIR=MFLD_PRx
+		FILE_NAME="*fastboot*.zip"
 		;;
 	r3)
 		THIS_RELEASE=$R3_RELEASE
 		THIS_URL=$R3_URL
 		THIS_DIR=~/DB/R3/$R3_RELEASE
+		PLAT_DIR=MFLD_PRx
+		FILE_NAME="*fastboot*.zip"
 		;;
+	cv)
+		THIS_RELEASE=$CV_RELEASE
+		THIS_URL=$CV_URL
+		THIS_DIR=~/DB/CV/$CV_RELEASE
+		PLAT_DIR=CTP_PR0
+		FILE_NAME="ctp_vv-VV-build-eng-system.zip"
+		;;
+	
 	esac
 	
 	if [ -e $THIS_DIR ]; then
@@ -179,61 +176,88 @@ get(){
 	mkdir -p $THIS_DIR
 	cd $THIS_DIR
 	print "get system file."
-	wget -r -l1 --no-parent -A"*fastboot*.zip"  http://$THIS_URL/$THIS_RELEASE/MFLD_PRx/flash_files/build-eng/
-	mv $THIS_URL/$THIS_RELEASE/MFLD_PRx/flash_files/build-eng/* .
+	wget -r -l1 --no-parent -A$FILE_NAME  http://$THIS_URL/$THIS_RELEASE/$PLAT_DIR/flash_files/build-eng/
+	mv $THIS_URL/$THIS_RELEASE/$PLAT_DIR/flash_files/build-eng/* .
 	rm -rf jfumgbuild-depot.jf.intel.com
 	print "get PR3.1 blankphone."
-	wget -r -l1 --no-parent -A"*PR3.1*.zip" http://$THIS_URL/$THIS_RELEASE/MFLD_PRx/flash_files/blankphone/
-	mv $THIS_URL/$THIS_RELEASE/MFLD_PRx/flash_files/blankphone/* .
+	wget -r -l1 --no-parent -A"*blankphone.zip" http://$THIS_URL/$THIS_RELEASE/$PLAT_DIR/flash_files/blankphone/
+	mv $THIS_URL/$THIS_RELEASE/$PLAT_DIR/flash_files/blankphone/* .
 	rm -rf jfumgbuild-depot.jf.intel.com
+
+}
+
+build_all(){
+	
+	print "BUILD ctp_pr0-eng"
+	vendor/intel/support/build_all.sh -c ctp_pr0
+	mv ./out ./out_ctp_pr0
+
+	print "BUILD mfld_pr2"
+        vendor/intel/support/build_all.sh -c mfld_pr2
+	mv ./out ./out_mfld_pr2	
 
 }
 
 #SYNC REPO TO A RELEASE
 sync(){
-
+	set_dir $1
 	cd $DIR
-
 	case $1 in 
-	r2) 	wget http://jfumgbuild-depot.jf.intel.com/build/eng-builds/mfld-r2/android/gingerbread-platform/releases/$R2_RELEASE/manifest-$R2_RELEASE-generated.xml
+	r2) 	wget http://$R2_URL/$R2_RELEASE/manifest-$R2_RELEASE-generated.xml
 		RELEASE=$R2_RELEASE 
 		;;
-	r3)	 wget	http://jfumgbuild-depot.jf.intel.com/build/eng-builds/mfld-r3/android/ice-cream-sandwich-platform/releases/$R3_RELEASE/manifest-$R3_RELEASE-generated.xml
+	r3)	 wget	http://$R3_URL/$R3_RELEASE/manifest-$R3_RELEASE-generated.xml
 		RELEASE=$R3_RELEASE
 		;;
 	*)exit 1;;
 	esac
+	print "sync $1 to $RELEASE"
 
 	cp ./manifest-$RELEASE-generated.xml ./.repo/manifests
 	sed -i 's/jfumg-gcrmirror.jf.intel.com/ncsgit001.nc.intel.com/g' ./.repo/manifests//manifest-$RELEASE-generated.xml
 	cd $DIR
 	repo init -m manifest-$RELEASE-generated.xml 
 	repo sync -d
-}
-sync_clean_build_full()
-{
-	sync r3
-	clean_build_full r3
+
+	build_all
 }
 
-scratch_r3()
+scratch()
 {
+	set_dir $1
+	cd $DIR
 	
-	print "SCRATCH R3"
+	print "SCRATCH $1"
 
-	if [ -e ~/RELEASES/R3/$R3_RELEASE ]; then
-		print "$R3_RELEASE BUILD EXISTS PLEASE REMOVE IT."
+	case $1 in 
+	r2)	NEW_BUILD_DIR=~/RELEASES/R2/$R2_RELEASE 	
+		RELEASE=$R2_RELEASE
+		MANIFEST=$R2_MANIFEST
+		;;
+	r3)	NEW_BUILD_DIR=~/RELEASES/R3/$R3_RELEASE
+		RELEASE=$R3_RELEASE
+		MANIFEST=$R3_MANIFEST
+		;;
+	cv)     NEW_BUILD_DIR=~/RELEASES/R3/$CV_RELEASE
+		RELEASE=$CV_RELEASE
+		MANIFEST=$CV_MANIFEST
+		;;
+	*)exit 1;;
+	esac
+
+	if [ -e $NEW_BUILD_DIR ]; then
+		print "$RELEASE BUILD EXISTS PLEASE REMOVE IT."
 		exit
 	fi
 
-        mkdir -p ~/RELEASES/R3/$R3_RELEASE
-        cd ~/RELEASES/R3/$R3_RELEASE
+        mkdir -p $NEW_BUILD_DIR
+        cd $NEW_BUILD_DIR
 
 	print "REPO INIT"
 	repo init -u git://android.intel.com/manifest -b platform/android/main -m android-main
 	
 	print "GET MANIFEST"
-        wget $R3_MANIFEST
+        wget $MANIFEST
         MANIFEST=`ls *.xml`
         sed -i 's/jfumg-gcrmirror.jf.intel.com/ncsgit001.nc.intel.com/g' ./$MANIFEST
 
@@ -245,72 +269,64 @@ scratch_r3()
 	print "REPO SYNC"
         repo sync
 
-	print "BUILD"
-        vendor/intel/support/build_all.sh -c mfld_pr2
+	build_all
 }
 
-scratch_r2()
-{
-	print "SCRATCH R2"
-	if [ -e ~/RELEASES/R2/$R2_RELEASE ]; then
-		print " ~/RELEASES/R2/$R2_RELEASE BUILD EXISTS PLEASE REMOVE IT."
-		exit
-	fi
-
-        mkdir -p ~/RELEASES/R2/$R2_RELEASE
-        cd ~/RELEASES/R2/$R2_RELEASE
-	repo init -u git://android.intel.com/manifest -b gingerbread -m stable
-
-	print "GET MANIFEST"
-	wget $R2_MANIFEST
-	MANIFEST=`ls *.xml`
-	sed -i 's/jfumg-gcrmirror.jf.intel.com/ncsgit001.nc.intel.com/g' ./$MANIFEST
-	cp  $MANIFEST ./.repo/manifests
-
-	print " REPO INIT"
-	repo init -m  $MANIFEST
-	print "REPO SYNC"
-	repo sync
-	print "BUILD"
-	vendor/intel/support/build_all.sh -c mfld_pr2
-
-
-
-}
 
 usage(){
-	echo 	" usage is:
-		where x is r2 || r3
-		-k) build_kernel:
-			x: incremental kernel-only build
-			boottarball_x: kernel + modules build
-			example:
-				mki.sh -k boottarball_r3
-				mki.sh -k r2
-		
-		-fk) flash_kernel
-			flash currently built boot.img
-			example:
-				mki.sh -fk r3
-				mki.sh -fk r2
-		-s_x) get code and build EVERYTHING!
-			example:
-				mki.sh -s_r3
-				mki.sh -s_r2
+echo "usage is:
 
-			requires these set on mki.sh, check inside for example!
-			R2_RELEASE
-			R2_URL
-			R2_MANIFEST
-					
-		--full_andrid_build x) runs make at top dir
-			example:
-				mki.sh --full_andrid_build r3
-
-		--get_release x) get DB for flash.
-			example:	
-				mki.sh --get_release r3
-		"
+-k) build_kernel: (mki.sh -k boottarball_r3 || mki.sh -k r3)
+-f) flash_kernel: (mki.sh -fk r3)
+-g) get code and build: (mki.sh -g)
+-b) runs make at top dir: mki.sh -f r3
+-d) Download prebuilt binaries. mki.sh -d r3
+-p) make r3 package from local build
+"
+}
+make_package()
+{
+	set_dir $1
+	cd $DIR
+	case $1 in
+	r2)	RELEASE=$R2_RELEASE
+		DB_DIR_FILES=~/DB/R2/$RELEASE/mfld_prx-eng-fastboot*
+		DB_DIR=~/DB/R2
+		OUT_DIR=$OUT_PLATFORM
+		;;
+	r3)	RELEASE=$R3_RELEASE
+		DB_DIR_FILES=~/DB/R3/$RELEASE/mfld_prx-eng-fastboot*
+		DB_DIR=~/DB/R3
+		OUT_DIR=$OUT_PLATFORM
+		;;
+	cv)	RELEASE=$CV_RELEASE
+		DB_DIR_FILES=~/DB/CV/$RELEASE/ctp_vv-VV-build-eng-system*
+		DB_DIR=~/DB/CV
+		OUT_DIR=out/target/product/ctp_pr0
+		;;
+	*)exit 1;;
+	esac
+	if [ ! -e $DB_DIR/$RELEASE ]; then
+		print "error $DB_DIR/$RELEASE does not exit."
+		exit 1;
+	fi
+	cd $OUT_DIR
+	rm -rf my_pkg
+	mkdir my_pkg
+	cd my_pkg
+	cp ../boot.bin ./
+	cp ../system.tar.gz ./
+	cp ../radio_firmware.bin ./
+	cp ../recovery.img ./
+	cp $DB_DIR_FILES/IFWI* ./
+	cp $DB_DIR_FILES/SIGNED* ./
+	cp $DB_DIR_FILES/flash.xml ./
+	cp $DB_DIR_FILES/kboot.bin ./
+	rm my_$1_build.zip
+	zip my_$1_build.zip *
+	rm -rf $DB_DIR/MY_BUILD
+	mkdir $DB_DIR/MY_BUILD
+	cp my_$1_build.zip $DB_DIR/MY_BUILD
 }
 
 make_shit(){
@@ -327,6 +343,22 @@ make_shit(){
 	
 
 }
+make_shit2(){
+	cd $R3_DIR
+	source build/envsetup.sh
+	lunch mfld_pr2-eng
+	cd frameworks/base/services/java/com/android/server
+	mm
+	cd $R3_DIR/frameworks/base/services/jni
+	mm
+	cd $R3_DIR
+	adb root
+	adb remount
+	adb push out/target/product/mfld_pr2/system/lib/libandroid_servers.so /system/lib/libandroid_servers.so
+	adb push out/target/product/mfld_pr2/system/framework/services.jar /system/framework/services.jar
+	adb reboot
+
+}
 
 if [ -z "$1" ]; then
 	usage
@@ -339,29 +371,37 @@ while [ ! -z "$1" ]; do
 	-k)
 		build_kernel $2;
 		break;;
-	-fk)
+	-f)
 		flash_kernel $2;
 		break;;
-	--get_release)
+	-d)
 		get $2;
 		break;;
-	--full_andrid_build)
+	-b)
 		full_andrid_build $2;
 		break;;
-	-s_r3)
-		scratch_r3
+	-g)
+		scratch $2
 		break;;
-	-s_r2)
-		scratch_r2
+	-p)
+		make_package $2
 		break;;
 	-v)
 		make_shit
+		break;;
+	-v2)
+		make_shit2
 		break;;	
+	-h)
+		usage
+		shift
+		break;;
 	*)
 		usage
 		break;;
 
   esac
+	echo "aa"
   shift
 done
 
