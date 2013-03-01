cde0e0f5dfb17cbc3fa84de3eb903f24d4c7e692
Axel Haslam <axelx.haslam@intel.com>
2013-03-01 10:28:21 +0100

    gpio_conf
    Signed-off-by: Axel Haslam <axelx.haslam@intel.com>

--- gpio_conf.c
+++ gpio_conf.c
@@ -42,36 +42,45 @@
 #define GPIO_GAFR1_U	0x60
 #define GPIO_GAFR2_L	0x64
 #define GPIO_GAFR2_U	0x68
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#define MAX_SIZE 10000
+#define GPIO_GFBR0	0x6C
+#define GPIO_GFBR1	0x70
+#define GPIO_GFBR2	0x74
+#define GPIO_GPIT0	0x78
+#define GPIO_GPIT1	0x7C
+#define GPIO_GPIT2	0x80
+#define MAX_SIZE	100000
 
 #define DEBUG
 
 
 #ifdef DEBUG
-	#define pr_dbg(...) printf(__VA_ARGS__);
+	#define pr_dbg(...) printf("%s %d ", __FUNCTION__,__LINE__);printf(__VA_ARGS__);
 #else
 	#define pr_dbg(...) {while(0)};	
 #endif
 
 /******************************************\
 
-	Text parsing
+	Input Text parsing
 
 \*******************************************/
+
+unsigned char ischar(char data)
+{
+	int i = 0;
+	if ('0' <= data && data <= '9') {
+    		i = data - '0';
+	} else if ('a' <= data && data <= 'f') {
+ 		i = 10 + data - 'a';
+	} else if ('A' <= data && data <= 'F') {
+		i = 10 + data - 'A';
+	} else if ('[' == data || data == ']') {
+	} else {
+		return false;
+	}
+	return true;
+}
+
 
 int print_buff(unsigned char *data, int size)
 {
@@ -83,7 +92,18 @@ int print_buff(unsigned char *data, int size)
 	return 0;
 }
 
-int remove_address(char *data)
+int p_buff_int(unsigned char *data, int size) 
+{
+	int i = 0;
+	unsigned int *pl = (unsigned int *)data;
+	for (i = 0 ; i < size/4 ; i++) {
+		printf("data[%x] = 0x%08x\n",i*4,*(pl+i));	
+	}
+	return 0;
+}
+
+
+int remove_junk(char *data, int size)
 {
 	int i = 0, j = 0;
 	bool ignore = false;
@@ -93,6 +113,9 @@ int remove_address(char *data)
 
 	for (i = 0; i < MAX_SIZE; i++) {
 		char p = data[i];
+		if (!ischar(p))
+			continue;
+
 		if (p == '[') {
 			ignore = true;
 			continue;
@@ -103,27 +126,6 @@ int remove_address(char *data)
 		}
 		if (!ignore)
 			data_tmp[j++] = p;
-		if (p == '\0')
-			break;
-	}
-
-	strcpy(data, data_tmp);	
-	pr_dbg("Output:\n%s\n",data);
-	return 0;
-}
-
-int remove_space(char *data)
-{
-	int i = 0, j = 0;
-	char data_tmp[MAX_SIZE] ;
-
-	pr_dbg("input:\n %s\n",data);
-	for (i = 0; i < MAX_SIZE; i++) {
-		char p = data[i];
-		if ((p == ' ') || (p == '\n') ) {
-			continue;
-		}
-		data_tmp[j++] = p;
 		if (p == '\0')
 			break;
 	}
@@ -168,7 +170,7 @@ int reorder_bytes(unsigned char *data, int size)
 
 	return 0;
 }
-int buff_char2hex(unsigned char *data, int *size1)
+int buff_char2hex(unsigned char *data, int size1)
 {
 	int i = 0, j = 0, size = 0;
 	unsigned char data_tmp[MAX_SIZE] ;
@@ -192,6 +194,7 @@ int buff_char2hex(unsigned char *data, int *size1)
 	pr_dbg("3 Input:\n",data);
 	print_buff(data, size);
 
+	/* 2 nibbles  = 2 charecters = char = 1 byte */
 	size /= 2;
 	for (i = 0, j = 0; i < size ; i++ ) {
 		unsigned char p = data[j++];
@@ -199,7 +202,8 @@ int buff_char2hex(unsigned char *data, int *size1)
 		p |= p2 << (NIBBLE);
 		data_tmp[i] = p;
 	}
-	
+
+	/*copy it back to the buff*/	
 	for (i = 0; i < size; i++)
 		data[i] = data_tmp[i];
 
@@ -212,7 +216,7 @@ int buff_char2hex(unsigned char *data, int *size1)
 
 /******************************************\
 
-	show funcions
+	Tool funcions
 
 \*******************************************/
 
@@ -224,144 +228,129 @@ bool is_bit_set(unsigned int data, unsigned int pos)
 	return false;
 }
 
-int show_gpio(unsigned int add, char *data, unsigned int size)
+/******************************************\
+
+	GPIO funcions
+
+\*******************************************/
+int show_gpio_g(int offset, unsigned char *p)
 {
-	int i = 0, j = 0;	
+	int gpio = 0, j = 0, i = 0;
+	unsigned int *pl = (unsigned int *) (p);	
 
-	printf("\nGPIO Pin Level Registers:\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * 2); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i,
-				is_bit_set(data[(GPIO_GPLR0 * BYTES_IN_CHAR) + j], i)? "HIGH":"LOW");
-			printf("GPIO_%d %s ", (j * CHAR) + i + REG_SIZE,
-				is_bit_set(data[(GPIO_GPLR1 * BYTES_IN_CHAR) + j], i)? "HIGH":"LOW");
-			printf("GPIO_%d %s\n", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GPLR2 * BYTES_IN_CHAR)+ j], i)? "HIGH":"LOW");
+	pl += (offset / 4);
+	pr_dbg("show_gpio_g data: %08x\n", *(pl + 0));
+	pr_dbg("show_gpio_g data: %08x\n", *(pl + 1));
+	pr_dbg("show_gpio_g data: %08x\n", *(pl + 2));
+
+	for (gpio = 0; gpio < N_GPIOS / 3; gpio++ ) {
+		for (i = 0; i < 3; i++) {
+			if (gpio == 15) {
+				printf("GPIO_%d %s ",
+					(gpio + (i * REG_SIZE)),
+					is_bit_set(*(pl + i),
+					gpio) ? "R":"R");
+				continue;
+			}
+			printf("GPIO_%d %s ",
+				(gpio + (i * REG_SIZE)),
+				is_bit_set(*(pl + i),
+				gpio) ? "1":"0");
 		}
-
-	printf("\nGPIO Pin Direction Register:\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * BYTES_IN_CHAR); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 0),
-				is_bit_set(data[(GPIO_GPDR0 * BYTES_IN_CHAR) + j], i)? "OUT":"IN");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 1),
-				is_bit_set(data[(GPIO_GPDR1 * BYTES_IN_CHAR) + j], i)? "OUT":"IN");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GPDR2 * BYTES_IN_CHAR)+ j], i)? "OUT":"IN");
-			printf("\n");
-		}	
-
-	printf("\nGPIO Pin-Output Set Registers\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * BYTES_IN_CHAR); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 0),
-				is_bit_set(data[(GPIO_GPSR0 * BYTES_IN_CHAR) + j], i)? "R":"R");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 1),
-				is_bit_set(data[(GPIO_GPSR1 * BYTES_IN_CHAR) + j], i)? "R":"R");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GPSR2 * BYTES_IN_CHAR)+ j], i)? "R":"R");
-			printf("\n");
-		}	
-
-	printf("\nGPIO Pin-Output Clear Registers\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * BYTES_IN_CHAR); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 0),
-				is_bit_set(data[(GPIO_GPCR0 * BYTES_IN_CHAR) + j], i)? "R":"R");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 1),
-				is_bit_set(data[(GPIO_GPCR1 * BYTES_IN_CHAR) + j], i)? "R":"R");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GPCR2 * BYTES_IN_CHAR)+ j], i)? "R":"R");
-			printf("\n");
-		}	
-
-	printf("\nGPIO Rising-Edge Detect Enable Registers\n");
-	printf("1 = Sets corresponding GEDR status bit when a rising edge is detected\n");
-	printf("0 = Disables rising-edge detect enable.\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * BYTES_IN_CHAR); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 0),
-				is_bit_set(data[(GPIO_GRER0 * BYTES_IN_CHAR) + j], i)? "ON":"OFF");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 1),
-				is_bit_set(data[(GPIO_GRER1 * BYTES_IN_CHAR) + j], i)? "ON":"OFF");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GRER2 * BYTES_IN_CHAR)+ j], i)? "ON":"OFF");
-			printf("\n");
-		}
-
-	printf("\nFalling-Edge Detect Enable Registers:\n");
-	printf("0 = Disables rising-edge detect enable.\n");
-	printf("1 = Sets corresponding GEDR status bit when a falling edge is detected on the GPIO pin.\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * BYTES_IN_CHAR); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 0),
-				is_bit_set(data[(GPIO_GFER0 * BYTES_IN_CHAR) + j], i)? "ON":"OFF");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 1),
-				is_bit_set(data[(GPIO_GFER1 * BYTES_IN_CHAR) + j], i)? "ON":"OFF");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GFER2 * BYTES_IN_CHAR)+ j], i)? "ON":"OFF");
-			printf("\n");
-		}
-
-	printf("\nGPIO Pin Edge Detect Status:\n");
-	printf("0 = No edge detect has occurred on pin as specified in GRER and/or GFER.\n");
-	printf("1 = Edge detect has occurred on pin as specified in GRER and/or GFER.\n");
-	for (j = 0; j < ((REG_SIZE / BYTE) * BYTES_IN_CHAR); j++)
-		for (i = 0; i < (CHAR); i++) {
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 0),
-				is_bit_set(data[(GPIO_GEDR0 * BYTES_IN_CHAR) + j], i)? "ON":"OFF");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 1),
-				is_bit_set(data[(GPIO_GEDR1 * BYTES_IN_CHAR) + j], i)? "ON":"OFF");
-			printf("GPIO_%d %s ", (j * CHAR) + i + (REG_SIZE * 2),
-				is_bit_set(data[(GPIO_GEDR2 * BYTES_IN_CHAR)+ j], i)? "ON":"OFF");
-			printf("\n");
-		}
-	
-	printf("\nGPIO Alternate Function Select:\n");
-	for (j = 0; j < N_GPIOS; j++) {
-		int reg = 0;
-		int index = j / 14;	
-		unsigned int value = 0;
-		unsigned int mask = 0;
-
-		switch (index) {
-		case 0:	reg = GPIO_GAFR0_L;
-		case 1: reg = GPIO_GAFR0_U;
-		case 2: reg = GPIO_GAFR1_L;
-		case 3: reg = GPIO_GAFR1_U;
-		case 4: reg = GPIO_GAFR2_L;
-		case 5: reg = GPIO_GAFR2_U;
-		}
-		/*4 bytes 8 characters*/
-		for(i = 0; i < 8; i++)
-			value |= data[reg + i] << (4 * i);
-
-		mask = 0x3 << ((j % 16) * 2);
-	
-		printf("GPIO_%d %x %x\n",j, value, value & mask);
+		printf("\n");
 	}
-	
+	return 0;
 
+}
+
+int show_gpio_alt(int offset, unsigned char *p, int gpio_base)
+{
+	int gpio = 0, j = 0, i = 0;
+	unsigned int *pl = (unsigned int *)(p);
+
+	pl += (offset / 4);
+	pr_dbg("show_gpio_alt data: %08x\n", *(pl + 0));
+
+	for (gpio = 0; gpio < 15; gpio++) {
+		unsigned int data = *(pl);
+
+		/* select only interesting bits */
+		data &= (0x3 << (gpio * 2));
+		data = (data >> (gpio * 2));
+		printf("GPIO_%d FUNC %d\n", gpio + gpio_base, data);
+	}	
 
 	return 0;
 }
+int gpio_dump(char *data)
+{
+	unsigned char *p = (unsigned char *)data;
 
+	printf("\nGPIO Pin Level Registers:\n");
+	show_gpio_g(GPIO_GPLR0, p);
+	printf("\nGPIO Pin Direction Register:\n");
+	show_gpio_g(GPIO_GPDR0, p);
 
+	printf("\nGPIO Pin-Output Set Registers:\n");
+	show_gpio_g(GPIO_GPSR0, p);
+	printf("\nGPIO Pin-Output Clear Registers:\n");
+	show_gpio_g(GPIO_GPCR0, p);
+	printf("\nGPIO Rising-Edge Detect Enable Registers:\n");
+	show_gpio_g(GPIO_GRER0, p);
+	printf("\nGPIO Falling-Edge Detect Enable Registers:\n");
+	show_gpio_g(GPIO_GFER0, p);
+	printf("\nGPIO Edge Detect Status Register:\n");
+	show_gpio_g(GPIO_GEDR0, p);
+	printf("\nGPIO Function Registers:\n");
+	show_gpio_alt(GPIO_GAFR0_L, p, 16 * 0);
+	show_gpio_alt(GPIO_GAFR0_U, p, 16 * 1);
+	show_gpio_alt(GPIO_GAFR1_L, p, 16 * 2);
+	show_gpio_alt(GPIO_GAFR1_U, p, 16 * 3);
+	show_gpio_alt(GPIO_GAFR2_L, p, 16 * 4);
+	show_gpio_alt(GPIO_GAFR2_U, p, 16 * 5);
+	printf("\nGPIO Glitch Filter Bypass Register:\n");
+	show_gpio_g(GPIO_GFBR0, p);
+	printf("\nGPIO Interrupt type Register:\n");
+	show_gpio_g(GPIO_GPIT0, p);
+	return 0;
+}
+
+/******************************************\
+
+	MAIN
+
+\*******************************************/
 int main()
 {
 	
-	char input[100000] = 
-"[ff119000] 51 a0 00 c0 45 12 48 c6 3e c0 20 76 00 00 a7 00 10 c1 8b 0e 00 40 00 1c 00 00 00 00 00 00 00 00 \
-[ff119080] 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 09 00 00 c0 0a 28 00 10 20 10 00 00 05 00 00 c0 \
-[ff119100] 05 0a 00 e0 20 a1 00 40 00 00 00 00 00 00 00 00 00 00 00 00 00 55 55 1a 95 56 55 05 00 00 00 00 \
-[ff119180] 00 00 00 00 00 10 a4 00 55 00 00 00 04 00 00 00 00 00 00 00 00 00 00\0" ;	
+	char *input;
 	int size = 0;
+	FILE *fp;
+	int i = 0;
 
-	remove_address(input);
-	remove_space(input);
-	buff_char2hex(input, &size);
-	
-//	show_gpio(GPIO_BASE, input, size);
+
+	fp = fopen("/home/axelh/tools/axeltest.txt","w");
+	fprintf(fp,"%s","r1 mmio 0xff119000 0x77");
+	fclose(fp);
+
+	fp = fopen("/home/axelh/tools/axeltest2.txt","r");
+	/* find the size */	
+	fseek(fp, 0L, SEEK_END);
+	size = ftell(fp);
+	fseek(fp, 0L, SEEK_SET);	
+	input = malloc(size);
+	/* read into buffer */
+	while (fscanf(fp, "%c",&input[i++]) != EOF) {
+		printf("%c",input[i -1]);
+	}
+	fclose(fp);
+
+	remove_junk(input, size);
+	buff_char2hex(input, size);
+	gpio_dump(input);
+
 	printf("\ndone.\n");
+out:
+	free(input);
 	return 0;
 }
